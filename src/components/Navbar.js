fb18ce3038a834c63d67dd2374bf3f74745c1169
Adrian Camacho <adriancamacho18@gmail.com>
2022-01-01 12:08:14 -0800

    add css styling sheet to app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutLined, MoneyCollectOutlined, BulbOutLined, FundOutLined, MenuOutLined } from '@ant-design/icons';
+import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar />
+        <Avatar src={icon} size="large"/>
         <Typography.Title level={2} className="logo">
           <Link to="/">Crypto Coinz</Link>
         </Typography.Title>
