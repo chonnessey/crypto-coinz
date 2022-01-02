57fd0ab7f3ad7ff723622033b252238a04b74de4
Adrian Camacho <adriancamacho18@gmail.com>
2022-01-02 13:02:39 -0800

    add successful data query for top 50 coins

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,6 +3,8 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import CryptoCurrencies from './CryptoCurrencies';
+import News from './News';
 
 const { Title } = Typography;
 
@@ -22,6 +24,16 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Crypto Currencies in the World</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+      </div>
+      <CryptoCurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+      </div>
+      <News simplified />
     </>
   )
 }
