8381fc643a9674c0532737432e7e0af68d89efc3
Adrian Camacho <adriancamacho18@gmail.com>
2022-01-02 17:31:18 -0800

    finish logic for crypto currencies component

--- src/components/CryptoCurrencies.js
+++ src/components/CryptoCurrencies.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const CryptoCurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+const CryptoCurrencies = ( { simplified } ) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [searchTerm, setSearchTerm] = useState('');
 
   console.log(cryptos);
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    setCryptos(filteredData)
+  }, [cryptosList, searchTerm]);
 
   if(isFetching) return 'Loading...';
 
   return (
     <>
-      <Row gutters={[32, 32]} className="crypto-card-container">
-        {cryptos.map((currency) => (
+      {!simplified && (
+        <div className="search-crypto">
+        <Input 
+          placeholder="Search Crypto Currencies"
+          onChange={(e) => setSearchTerm(e.target.value)}/>
+      </div>
+      )}
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card 
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -9,7 +9,7 @@ import News from './News';
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   console.log(data);
   const globalStats = data?.data?.stats;
 
